adc476897a1d1f1e579b4931af0c61f4f9a553f2
Karib <abdurrahmanfarukkhan@gmail.com>
2024-09-25 13:41:55 +0600

    audio file playing

--- scripts/sripts.js
+++ scripts/sripts.js
@@ -1,1 +1,36 @@
-let audioelement = new Audio();
+let audioelement = new Audio("../assets/songs/song1.m4a");
+let songIndex = 0;
+const gif = document.getElementById("gif");
+
+let songList = [
+  {
+    filename: "Blinding Lights",
+    filePtah: "../assets/songs/song1.m4a",
+    coverPath:
+      "../assets/images/andrea-joshua-sibabalo-qoqonga-4XlsGQAWkNA-unsplash.jpg",
+  },
+];
+
+const masterPlay = document.getElementById("master_play");
+
+masterPlay.addEventListener("click", () => {
+  if (audioelement.paused || audioelement.currentTime <= 0) {
+    audioelement.play();
+    masterPlay.innerHTML = `<i class=" fa-3x fa-solid fa-pause"></i>`;
+    gif.style.opacity = "1";
+  } else {
+    audioelement.pause();
+    masterPlay.innerHTML = `<i class="fa-3x fa-solid fa-play play_button"></i
+        >`;
+    gif.style.opacity = "0";
+  }
+});
+
+const progressBar = document.getElementById("progress_bar");
+audioelement.addEventListener("timeupdate", () => {
+  let progress = Math.round(
+    (audioelement.currentTime / audioelement.duration) * 100
+  );
+  console.log(progress);
+  progressBar.value = progress;
+});
